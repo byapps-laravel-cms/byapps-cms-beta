fc05e7a4a5a9eef8cb541af04349d844a2061271
2jiwon <ljwjulian@gmail.com>
2019-10-02 15:44:28 +0900

    주문요청현황 함수 날짜 부분 수정

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -49,9 +49,10 @@ class HomeController extends Controller
 
         // 주문요청현황 데이터
         $preData = new StatusController;
+        // 임시날짜로 테스트
         $appsOrderCount = $preData->onGetAppsOrderCount();
-        $appendixOrderCount = $preData->onGetAppendixOrderCount();
-        $updateCount = $preData->onGetUpdateCount();
+        $appendixOrderCount = $preData->onGetAppendixOrderCount('2019-03-07');
+        $updateCount = $preData->onGetUpdateCount('2019-03-07');
 
         // 만료예정 데이터
         $preData = new ExpiredController;
--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -12,30 +12,61 @@ use App\QnaNonmember;
 
 class StatusController extends Controller
 {
-  /* 주문(앱)접수 */
-  public function onGetAppsOrderCount()
+  /* 어제 대비 퍼센트 계산 */
+  public function calculatePercent()
   {
+
+  }
+
+
+  /* 주문(앱)접수 */
+  /* date = yyyy-mm-dd  */
+  public function onGetAppsOrderCount($date = '')
+  {
+    // 넘겨받는 날짜가 있으면 해당 날짜를 unix_timestamp로 변경. 없으면 오늘 날짜를 계산
+    if ($date == '') {
+      $todate = time();
+    } else {
+      $todate = strtotime($date);
+    }
+
+    //info(date('Y-m-d', $todate));
+
     // AppsOrderData::where('app_process', 1)->where('reg_time', '>=', time())->count();
-    $appsOrderCount = AppsOrderData::where('app_process', 1)->count();
+    $appsOrderCount = AppsOrderData::where('app_process', 1)->where('reg_time', '>=', $todate)->count();
 
     return $appsOrderCount;
   }
 
   /* 부가서비스 접수 */
-  public function onGetAppendixOrderCount()
+  public function onGetAppendixOrderCount($date = '')
   {
+    // 넘겨받는 날짜가 있으면 해당 날짜를 unix_timestamp로 변경. 없으면 오늘 날짜를 계산
+    if ($date == '') {
+      $todate = time();
+    } else {
+      $todate = strtotime($date);
+    }
+
     // AppendixOrderData::where('app_process', 1)->where('reg_time', '>=', time())->count();
-    $appendixOrderCount = AppendixOrderData::where('app_process', 1)->count();
+    $appendixOrderCount = AppendixOrderData::where('app_process', 1)->where('reg_time', '>=', $todate)->count();
 
     return $appendixOrderCount;
   }
 
   /* 업데이트 접수 */
-  public function onGetUpdateCount()
+  public function onGetUpdateCount($date = '')
   {
+    // 넘겨받는 날짜가 있으면 해당 날짜를 unix_timestamp로 변경. 없으면 오늘 날짜를 계산
+    if ($date == '') {
+      $todate = time();
+    } else {
+      $todate = strtotime($date);
+    }
+
     // original query: select sum(case when update_process='1' then 1 else 0 end) order_no, sum(case when update_process='2' then 1 else 0 end) confirm_no,  sum(case when update_process='3' then 1 else 0 end) app_no
     // from BYAPPS2015_apps_update_data where (update_process='1' or update_process='2' or update_process='3') and reg_time>='".$search_time."'
-    $updateCount = UpdateData::where('app_process', 1)->count();
+    $updateCount = UpdateData::where('app_process', 1)->where('reg_time', '>=', $todate)->count();
     // UpdateData::where('app_process', 1)->where('reg_time', '>=', time())->count();
 
     return $updateCount;
