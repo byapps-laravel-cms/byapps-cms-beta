e0a58dd05f2288e1c579a945eb4fcf4ddac87197
2jiwon <ljwjulian@gmail.com>
2019-10-21 20:04:03 +0900

    매출통계표 기간 계산 부분 수정(입력한 기간 대비)

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -5,18 +5,37 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Builder;
 
+use DateTime;
 use App\AppsData;
 use App\PaymentData;
 use App\AppendixOrderData;
 
 class SalesController extends Controller
 {
+    // 기간 계산
+    public function getTerm($start, $end)
+    {
+      $date1 = new DateTime($start);
+      $date2 = new DateTime($end);
+      $diff = $date2->diff($date1);
 
+      if ($diff->format('%a') == 0) {
+        return 1;
+      } else {
+        return $diff->format('%a');
+      }
+
+    }
     // 플랫폼 데이터
     public function getPlatformData(Request $request)
     {
         $start = $request->start;
         $end = $request->end;
+
+        $term = $this->getTerm($start, $end);
+
+        info(strtotime($start));
+        info((60*60*24*$term));
 
         // 이번주
         $thisTotal = AppsData::where('app_process', '!=', 8)
@@ -34,14 +53,14 @@ class SalesController extends Controller
 
         // 지난주
         $lastTotal = AppsData::where('app_process', '!=', 8)
-                           ->whereBetween('reg_time', [strtotime($start) - 604800 , strtotime($end) - 604800])
+                          ->whereBetween('reg_time', [strtotime($start) - (60*60*24*$term), strtotime($end) - (60*60*24*$term)])
                           ->count();
 
         $lastFree = AppsData::where('app_process', '!=', 8)
                           ->whereHas('payments', function (Builder $query) {
                             $query->where('amount', '<', 0);
                           })
-                          ->whereBetween('reg_time', [strtotime($start) - 604800, strtotime($end) - 604800])
+                          ->whereBetween('reg_time', [strtotime($start) - (60*60*24*$term), strtotime($end) - (60*60*24*$term)])
                           ->count();
 
         $lastPaid = $lastTotal - $lastFree;
--- resources/views/components/sales.blade.php
+++ resources/views/components/sales.blade.php
@@ -47,7 +47,7 @@
         <div class="input-group-append">
           <span class="input-group-text">
             <!-- <a href="javascript:void(0)" onclick="showEntireTable(day1, day2)"><i class="entypo-chart-bar"></i>보기</a> -->
-            <a href="javascript:void(0)" onclick="platformData()"><i class="entypo-chart-bar"></i>보기</a>
+            <a href="javascript:void(0)" onclick="showSalesTable()"><i class="entypo-chart-bar"></i>보기</a>
           </span>
         </div>
       </div>
@@ -64,7 +64,7 @@
       <thead>
         <tr>
             <th colspan="2" rowspan="2">구분</th>
-            <th colspan="3">이번주</th> <th colspan="3">지난주</th> <th colspan="3">증감수</th>
+            <th colspan="3" id="this">이번 주</th> <th colspan="3" id="last">지난 주</th> <th colspan="3">증감수</th>
         </tr>
         <tr>
             <th>전체</th> <th>유료</th> <th>무료</th>
@@ -99,9 +99,49 @@
 
 <script>
 $(document).ready(function() {
+  // 기본 표 출력
   platformData();
 });
 
+// 입력한 날짜 간 기간계산
+function getTerm() {
+  var start_date = $('#start_date_table').val();
+  var end_date = $('#end_date_table').val();
+  var diff = new Date(Date.parse(end_date) - Date.parse(start_date));
+
+  return Math.ceil(diff/1000/60/60/24);
+}
+
+// 표 출력하는 부분
+function showSalesTable() {
+  var start_date = $('#start_date_table').val();
+  var end_date = $('#end_date_table').val();
+
+  var term = getTerm();
+
+  var last_start = new Date(start_date);
+  var last_end = new Date(end_date);
+
+  last_start.setDate(last_start.getDate() - 1);
+  last_end.setDate(last_start.getDate() + term);
+
+  last_start = dateFormat(last_start, "yyyy-mm-dd");
+  last_end = dateFormat(last_end, "yyyy-mm-dd");
+
+  // 입력한 시작일과 종료일이 같으면 날짜는 한개만 출력
+  if(start_date == end_date) {
+    $('#this').html(start_date);
+    $('#last').html(last_start);
+  } else {  // 아니면 시작일, 종료일 출력
+    $('#this').html(start_date + " ~ " + end_date);
+    $('#last').html(last_start + " ~ " + last_end);
+  }
+
+  // 표 다시 출력
+  platformData();
+}
+
+// 표 출력하는 부분
 function platformData() {
   var startdate = '';
   var enddate = '';
@@ -168,4 +208,117 @@ function platformData() {
 function variation(val1, val2) {
   return val1 - val2;
 }
+
+// dateformat 부분
+var dateFormat = function () {
+    var token = /d{1,4}|m{1,4}|yy(?:yy)?|([HhMsTt])\1?|[LloSZ]|"[^"]*"|'[^']*'/g,
+        timezone = /\b(?:[PMCEA][SDP]T|(?:Pacific|Mountain|Central|Eastern|Atlantic) (?:Standard|Daylight|Prevailing) Time|(?:GMT|UTC)(?:[-+]\d{4})?)\b/g,
+        timezoneClip = /[^-+\dA-Z]/g,
+        pad = function (val, len) {
+            val = String(val);
+            len = len || 2;
+            while (val.length < len) val = "0" + val;
+            return val;
+        };
+
+    // Regexes and supporting functions are cached through closure
+    return function (date, mask, utc) {
+        var dF = dateFormat;
+
+        // You can't provide utc if you skip other args (use the "UTC:" mask prefix)
+        if (arguments.length == 1 && Object.prototype.toString.call(date) == "[object String]" && !/\d/.test(date)) {
+            mask = date;
+            date = undefined;
+        }
+
+        // Passing date through Date applies Date.parse, if necessary
+        date = date ? new Date(date) : new Date;
+        if (isNaN(date)) throw SyntaxError("invalid date");
+
+        mask = String(dF.masks[mask] || mask || dF.masks["default"]);
+
+        // Allow setting the utc argument via the mask
+        if (mask.slice(0, 4) == "UTC:") {
+            mask = mask.slice(4);
+            utc = true;
+        }
+
+        var _ = utc ? "getUTC" : "get",
+            d = date[_ + "Date"](),
+            D = date[_ + "Day"](),
+            m = date[_ + "Month"](),
+            y = date[_ + "FullYear"](),
+            H = date[_ + "Hours"](),
+            M = date[_ + "Minutes"](),
+            s = date[_ + "Seconds"](),
+            L = date[_ + "Milliseconds"](),
+            o = utc ? 0 : date.getTimezoneOffset(),
+            flags = {
+                d:    d,
+                dd:   pad(d),
+                ddd:  dF.i18n.dayNames[D],
+                dddd: dF.i18n.dayNames[D + 7],
+                m:    m + 1,
+                mm:   pad(m + 1),
+                mmm:  dF.i18n.monthNames[m],
+                mmmm: dF.i18n.monthNames[m + 12],
+                yy:   String(y).slice(2),
+                yyyy: y,
+                h:    H % 12 || 12,
+                hh:   pad(H % 12 || 12),
+                H:    H,
+                HH:   pad(H),
+                M:    M,
+                MM:   pad(M),
+                s:    s,
+                ss:   pad(s),
+                l:    pad(L, 3),
+                L:    pad(L > 99 ? Math.round(L / 10) : L),
+                t:    H < 12 ? "a"  : "p",
+                tt:   H < 12 ? "am" : "pm",
+                T:    H < 12 ? "A"  : "P",
+                TT:   H < 12 ? "AM" : "PM",
+                Z:    utc ? "UTC" : (String(date).match(timezone) || [""]).pop().replace(timezoneClip, ""),
+                o:    (o > 0 ? "-" : "+") + pad(Math.floor(Math.abs(o) / 60) * 100 + Math.abs(o) % 60, 4),
+                S:    ["th", "st", "nd", "rd"][d % 10 > 3 ? 0 : (d % 100 - d % 10 != 10) * d % 10]
+            };
+
+        return mask.replace(token, function ($0) {
+            return $0 in flags ? flags[$0] : $0.slice(1, $0.length - 1);
+        });
+    };
+}();
+
+// Some common format strings
+dateFormat.masks = {
+    "default":      "ddd mmm dd yyyy HH:MM:ss",
+    shortDate:      "m/d/yy",
+    mediumDate:     "mmm d, yyyy",
+    longDate:       "mmmm d, yyyy",
+    fullDate:       "dddd, mmmm d, yyyy",
+    shortTime:      "h:MM TT",
+    mediumTime:     "h:MM:ss TT",
+    longTime:       "h:MM:ss TT Z",
+    isoDate:        "yyyy-mm-dd",
+    isoTime:        "HH:MM:ss",
+    isoDateTime:    "yyyy-mm-dd'T'HH:MM:ss",
+    isoUtcDateTime: "UTC:yyyy-mm-dd'T'HH:MM:ss'Z'"
+};
+
+// Internationalization strings
+dateFormat.i18n = {
+    dayNames: [
+        "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat",
+        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+    ],
+    monthNames: [
+        "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+        "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+    ]
+};
+
+// For convenience...
+Date.prototype.format = function (mask, utc) {
+    return dateFormat(this, mask, utc);
+};
 </script>
