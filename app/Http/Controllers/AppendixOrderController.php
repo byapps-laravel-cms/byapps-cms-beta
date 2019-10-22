57853f5b048cb9f70b25e56f084121d88beefeae
2jiwon <ljwjulian@gmail.com>
2019-10-22 16:11:18 +0900

    부가서비스 접수 컨트롤러 작성

---
+++ app/Http/Controllers/AppendixOrderController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\AppendixOrderData;
+use Yajra\Datatables\Datatables;
+
+class AppendixOrderController extends Controller
+{
+  public function getIndex()
+  {
+      return view('appendixorderlist');
+  }
+
+  public function getAppendixOrderListData()
+  {
+    $appendixOrderListData = AppendixOrderData::select('idx',
+                                             'app_process',
+                                             'service_type',
+                                             'pay_way',
+                                             'receipt',
+                                             'order_name',
+                                             'app_company',
+                                             'cellno',
+                                             'reg_time');
+
+    return Datatables::of($appendixOrderListData)
+            ->setRowId(function($appendixOrderListData) {
+                return $appendixOrderListData->idx;
+            })
+            ->editColumn('reg_time', '{{ date("Y-m-d", $reg_time) }}')
+            ->orderColumn('reg_time', 'reg_time $1')
+            ->make(true);
+  }
+
+  public function getSingleData($idx)
+  {
+    $appendixOrderData = AppendixOrderData::where('idx', $idx)->first();
+
+    return view('appendixorderdetail')->with('appendixOrderData', $appendixOrderData);
+  }
+}
