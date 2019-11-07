8cad6d0f9b9856f777633378ad44b795fc2c0156
2jiwon <ljwjulian@gmail.com>
2019-11-07 19:05:59 +0900

    앱 카테고리 항목 제외

--- app/Http/Controllers/AppsOrderController.php
+++ app/Http/Controllers/AppsOrderController.php
@@ -18,7 +18,6 @@ class AppsOrderController extends Controller
   {
     $appsOrderData = AppsOrderData::select('idx',
                                          'app_process',
-                                         'app_cate',
                                          'app_name',
                                          'app_company',
                                          'order_name',
@@ -28,25 +27,24 @@ class AppsOrderController extends Controller
                                          'receipt',
                                          'reg_time');
 
+    $app_process = array("주문취소","접수","주문확인","개발진행","앱등록","서비스중지","서비스해지","","취소요청","접수대기");
+
     return Datatables::of($appsOrderData)
             ->setRowId(function($appsOrderData) {
               return $appsOrderData->idx;
             })
             ->editColumn('receipt', function($eloquent) {
-              return substr($eloquent->receipt, 0, 50);
+              if ($eloquent->receipt == '') {
+                return "미발행";
+              } else {
+              //  return substr($eloquent->receipt, 0, 50);
+                return str_replace("|","\n", $eloquent->receipt);
+              }
             })
-            // ->editColumn('pm_used', function($eloquent) {
-            //   if ($eloquent->pm_used == 0) return "미사용";
-            //   else return "사용 ".date('Y-m-d', $eloquent->used_time);
-            // })
-            // ->editColumn('pm_target', function($eloquent) {
-            //   if ($eloquent->pm_target == "ma") return "마케팅 오토메이션";
-            //   else return "앱 서비스";
-            // })
-            // ->editColumn('pm_content', function($eloquent) {
-            //   $preData = explode(":", $eloquent->pm_content);
-            //   return "월 ".number_format($preData[0])."원 지정결제";
-            // })
+            ->editColumn('app_process', function($eloquent) use ($app_process) {
+              return $app_process[$eloquent->app_process];
+            })
+
             ->editColumn('reg_time', '{{ date("Y-m-d", $reg_time) }}')
             ->orderColumn('reg_time', 'reg_time $1')
             ->make(true);
