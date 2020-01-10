be7b882c4bf0b864f4971a0643cf286d060e3d26
origin <jipark0716@gmail.com>
2020-01-10 13:16:41 +0900

    bug fix

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -9,20 +9,21 @@ class Authenticate
     public function handle($request, Closure $next, $guard = null)
     {
         $pageName = str_replace('.view','',\Request::route()->getName());
-        $white = ['admin','comment','commentsend'];
-        if(in_array($pageName,$white)){
-            return $next($request);
-        }
+        
         try {
             $per = request()->user()->adminNMNew;
         } catch (\Exception $e) {
-            return request()->ajax() ? response()->json(['message' => '권한이 없습니다.'], 400) : redirect(route('login'));
+            return request()->ajax() ? response()->json(['message' => '권한이 없습니다.'], 400) : redirect('/login');
         }
+
+        $white = ['admin','comment','commentsend','home'];
+        if(in_array($pageName,$white))
+            return $next($request);
 
         if (Auth::guard($guard)->check()) {
             if($per == 'all' || url()->current() == config('app.url') || strpos($per,'|'.$pageName.'|') > -1)
                 return $next($request);
         }
-        return request()->ajax() ? response()->json(['message' => '권한이 없습니다.'], 400) : redirect('/');
+        return request()->ajax() ? response()->json(['message' => '권한이 없습니다.'], 400) : abort(400,'권한이 없습니다.');
     }
 }
